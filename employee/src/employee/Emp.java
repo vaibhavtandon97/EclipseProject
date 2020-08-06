f284da5ca1f5b0c391e1da44dbfd3320ceb2eb2f
vaibhavtandon97 <vaibhavtandon97@gmail.com>
2020-08-07 02:28:24 +0530

    1st commit

---
+++ employee/src/employee/Emp.java
@@ -0,0 +1,15 @@
+package employee;
+
+public class Emp {
+
+	
+	public static void show() {
+		System.out.println("hello emp");
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		show();
+	}
+}
